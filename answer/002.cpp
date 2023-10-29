13e4d165da0cb0b1561344c2e176834a7c5f9384
LO5eZL <149298356+LO5eZL@users.noreply.github.com>
2023-10-29 20:05:25 +0800

    Update and rename 002.cpp to answer/002.cpp

--- answer/002.cpp
+++ answer/002.cpp
@@ -1,22 +1,5 @@
 #include <bits/stdc++.h>
 #define mian main
-#define ll long long
-#define elif else if
-
-using namespace std;
-
-struct
-{
-    bool o(bool x)
-    {
-        return x ? true : false;
-    }
-    bool O = true;
-} O;
-struct
-{
-    bool o = false;
-} o;
 
 int mian()
 {
